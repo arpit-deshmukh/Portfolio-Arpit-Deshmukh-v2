f40522c9c2231efe9ad4174101f306c7d914c387
Arpit Deshmukh <arpitdeshmukh21@gmail.com>
2026-07-19 22:25:26 +0530

    project section updated

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -14,15 +14,23 @@ const fadeUp = {
 };
 
 const projects = [
+  // {
+  //   title: "Niwaas – Rental Property Platform",
+  //   description:
+  //     "A rental property management platform using the MERN stack.",
+  //   tech: ["MERN", "JWT", "TailwindCSS"],
+  //   github:
+  //     "https://github.com/arpit-deshmukh/NIwaas-v3-Rental-Propery-management-Platform",
+  //   live: "https://niwaas-v3.vercel.app/",
+  //   image: "/project-assets/Niwaas-s1.png",
+  // },
   {
-    title: "Niwaas – Rental Property Platform",
-    description:
-      "A rental property management platform using the MERN stack.",
-    tech: ["MERN", "JWT", "TailwindCSS"],
-    github:
-      "https://github.com/arpit-deshmukh/NIwaas-v3-Rental-Propery-management-Platform",
-    live: "https://niwaas-v3.vercel.app/",
-    image: "/project-assets/Niwaas-s1.png",
+    title: "Smart CRM – Customer Relationship Management",
+    description: "A full-stack Customer Relationship Management (CRM) platform designed to help businesses manage leads, contacts, deals, companies, and communications efficiently with built-in analytics dashboard.",
+    tech: ["React", "Node.js", "Express", "MongoDB", "TailwindCSS", "Recharts"],
+    github: "https://github.com/arpit-deshmukh/smart-crm-TEW",
+    live: "https://smart-crm-tew.vercel.app/",
+    image: "/project-assets/smart-crm.png",
   },
   {
     title: "EcoPulse – Economy Insights Platform",
